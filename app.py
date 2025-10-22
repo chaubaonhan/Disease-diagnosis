feee7113d138bbd65be51e47378ceaccbeb1dcd2
Andy <126648776+chaubaonhan@users.noreply.github.com>
2025-10-22 12:57:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ Dự đoán nguy cơ mắc bệnh tim từ dữ liệu bệnh nhân với các m
 # --- PHẦN XỬ LÝ VÀ HIỂN THỊ DỮ LIỆU ---
 
 # Định nghĩa đường dẫn đến các file CSV
-db_file = 'C:\Users\Admin\Documents\GitHub\Disease-diagnosis\database\ptbxl_database.csv'
+db_file = 'Disease-diagnosis\database\ptbxl_database.csv'
 scp_file = 'D:\Demo\database\scp_statements.csv'
 
 # 1. Hiển thị dữ liệu từ ptbxl_database.csv
